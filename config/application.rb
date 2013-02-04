db952fbb5fdf9050029c986101d511123452a7e4
Nick Jerkovich <NickJerkovich@gmail.com>
2013-02-04 16:02:25 -0500

    installed devise, add a user,  and install devise view

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module RailsinstallerDemo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+  
+    config.assets.initialize_one_precompile = false
   end
 end
